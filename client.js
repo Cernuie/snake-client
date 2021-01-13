2dfea6cb7ba614e26c0187e6bc94337b3b3905ea
Alexander Nguyen <cernvii@gmail.com>
2021-01-13 18:09:30 +0000

    added setupInput

--- client.js
+++ client.js
@@ -26,6 +26,24 @@ const connect = function() {
 
 }
 
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    } 
+  })
+ 
+  return stdin;
+}
+setupInput();
 // "Move: up" - move up one square (unless facing down)
 // "Move: down" - move down one square (unless facing up)
 // "Move: left" - move left one square (unless facing right)
