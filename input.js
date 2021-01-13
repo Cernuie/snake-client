d2f3eff38e1ecbc71aeba71670832db309800470
Alexander Nguyen <cernvii@gmail.com>
2021-01-13 18:14:22 +0000

    moved things, added handleUserInput

--- input.js
+++ input.js
@@ -2,13 +2,17 @@ const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
-  stdin.resume();
+  stdin.resume(); 
+  return stdin;
+}
+
+const handleUserInput = function () {
   stdin.on('data', (key) => {
     if (key === '\u0003') {
       process.exit();
     } 
   })
- 
-  return stdin;
 }
-setupInput();
+handleUserInput();
+
+module.export = setupInput;
--- play.js
+++ play.js
@@ -1,8 +1,10 @@
 const net = require('net');
-
 /**
  * Establishes connection with the game server
  */
-const connect = require('./client.js');
+const connect = require('./client');
+const setupInput = require('./input');
 console.log('Connecting ...');
 connect();
+
+setupInput();
