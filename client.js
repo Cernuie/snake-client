02aaba7b9906e579fd8f5bedff4472c6a81e70ff
Alexander Nguyen <cernvii@gmail.com>
2021-01-13 18:06:33 +0000

    added one movement on connection

--- client.js
+++ client.js
@@ -15,10 +15,22 @@ const connect = function() {
   conn.on('connect', (connect) => {
     console.log("Successfully connected to game server.");
   })
-  conn.on('connect', () => {
-    conn.write("Name: AA");
+  conn.on('connect', (connect) => {
+    conn.write("Name: AN");
   });
+  conn.on('connect', (connect) => {
+    conn.write("Move: up");
+  })
+
   return conn;
+
 }
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
+
+
 
 module.exports = connect;
